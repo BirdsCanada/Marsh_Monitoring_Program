34d1dc2dacf5afa173c45578fbb28fc1f5b986f4
Danielle Ethier <dethier@birdscanada.org>
2026-05-19 14:29:45 -0400

    Start scripts

---
+++ Scripts/00_setup.R
@@ -0,0 +1,20 @@
+require(naturecounts)
+require(INLA)
+require(tidyverse)
+
+# Create folders as necessary
+if(!dir.exists("Data")) dir.create("Data")
+if(!dir.exists("Output")) dir.create("Output")
+if(!dir.exists("Output/Plots")) dir.create("Output/Plots")
+
+# set output directory for analysis files; create if not already there
+out.dir <- paste("./Output/", max.year, "/", sep = "")
+dir.create(out.dir, showWarnings=FALSE, recursive=TRUE)
+
+# set data directory for analysis files; create if not already there
+data.dir <- paste("./Data/", max.year, "/", sep = "")
+dir.create(data.dir, showWarnings=FALSE, recursive=TRUE)
+
+# set data directory for analysis files; create if not already there
+plot.dir <- paste("./Output/Plots/", max.year, "/", sep = "")
+dir.create(plot.dir, showWarnings=FALSE, recursive=TRUE)
---
+++ Scripts/DataExplore.R
@@ -0,0 +1,6 @@
+#Data Explore
+#May 2026
+
+#Determine what data we have in the database and the underlying formate
+
+source("Scripts/00_setup.R")
